b56564f7e6725a4d2a0a833e278f03ab3f685836
Aaron Gong <aaronjxz@gmail.com>
2018-02-23 17:57:03 +0000

    update to version 0.0.4

--- example/src/firebase.js
+++ example/src/firebase.js
@@ -1,10 +1,12 @@
 import Firebase from 'firebase'
 import 'firebase/firestore'
+import 'firebase/storage'
 import {firebaseCfg} from '../cfg.json'
 
 const firebaseApp = Firebase.initializeApp(firebaseCfg)
 
+const storage = firebaseApp.storage()
 const db = firebaseApp.database()
 const firestore = firebaseApp.firestore()
 const auth = firebaseApp.auth()
-export { db, auth, firestore }
+export { db, auth, firestore, storage }
