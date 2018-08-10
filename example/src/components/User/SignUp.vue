5496439107b254b58bea4a086db322b3308b3f7c
Aaron Gong <aaronjxz@gmail.com>
2018-08-11 00:46:00 +0800

    fix signUp.vue

--- example/src/components/User/SignUp.vue
+++ example/src/components/User/SignUp.vue
@@ -43,37 +43,37 @@
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        email: '',
-        password: '',
-        confirmPassword: ''
-      }
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: ''
+    }
+  },
+  computed: {
+    comparePasswords () {
+      return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
     },
-    computed: {
-      comparePasswords () {
-        return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
-      },
-      // can use vuex mapGetters
-      user () { return this.$store.getters.user },
-      error () { return this.$store.getters.error },
-      loading () { return this.$store.getters.loading }
-    },
-    watch: {
-      user (value) {
-        if (value !== null && value !== undefined) {
-          this.$router.push('/')
-        }
-      }
-    },
-    methods: {
-      onSignup () {
-        this.$store.dispatch('signUserUp', {email: this.email, password: this.password})
-      },
-      onDismissed () {
-        this.$store.dispatch('clearError')
+    // can use vuex mapGetters
+    user () { return this.$store.getters.user },
+    error () { return this.$store.getters.error },
+    loading () { return this.$store.getters.loading }
+  },
+  watch: {
+    user (value) {
+      if (value !== null && value !== undefined) {
+        this.$router.push('/')
       }
     }
+  },
+  methods: {
+    onSignup () {
+      this.$store.dispatch('signUserUp', {email: this.email, password: this.password})
+    },
+    onDismissed () {
+      this.$store.dispatch('clearError')
+    }
   }
+}
 </script>
