3a8eb52493ef40d5367c30ad307363b24fa8bb02
Aaron Gong <aaronjxz@gmail.com>
2019-04-24 14:53:05 +0800

    add subscription, todo authentication

---
+++ example-rest/src/pages/Category.vue
@@ -0,0 +1,193 @@
+<template>
+  <div>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <vue-crud-x
+          ref="category"
+          storeName="category"
+          :parentId="parentId"
+          v-bind="pageDefs"
+        >
+        </vue-crud-x>
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+<script>
+// import { makeCsvRow, exportCsv } from '@/assets/util'
+import { http } from '@/axios'
+import { apolloClient } from '@/graphql'
+import { GET_CATEGORIES, GET_CATEGORY, PATCH_CATEGORY, CATEGORY_UPDATED } from '@/queries'
+
+export default {
+  apollo: {
+    // getPosts: {
+    //   query: GET_POSTS,
+    //   result({ data, loading, networkStatus}) {
+    //     if (!loading) {
+    //       this.posts = data.getPosts
+    //       console.log('[networkStatus]', networkStatus)
+    //     }
+    //   },
+    //   error(e) {
+    //     console.error('[ERROR]', e)
+    //   }
+    // },
+    $subscribe: {
+      // When a post is added
+      categoryUpdated: {
+        query: CATEGORY_UPDATED,
+        result (data) {
+          // alert('POST ADDED', data.data.postAdded.title)
+          alert('SUBSCRIPTIONS: CATEGORY_UPDATED', data.data.categoryUpdated.id, data.data.categoryUpdated.name)
+        }
+      }
+    }
+  },
+  name: 'category',
+  data () {
+    return {
+      parentId: null,
+      pageDefs: {
+        crudTable: {
+          actionColumn: false,
+          addrowCreate: false,
+          // inline: false,
+          confirmCreate: true,
+          confirmUpdate: true,
+          confirmDelete: true,
+          headers: [
+            { text: 'Category Name', value: 'name', class: 'pa-1' }
+          ],
+          formatters: (value, _type) => value,
+          doPage: 2,
+          showFilterButton: false
+        },
+        crudFilter: { FilterVue: null, filterData: { } },
+        crudForm: {
+          FormVue: null,
+          formAutoData: {
+            id: { type: 'input', attrs: { hidden: true } }, // need id if there is delete
+            name: {
+              type: 'v-text-field',
+              attrs: {
+                label: 'Name',
+                rules: [v => !!v || 'Item is required']
+              },
+              halfSize: false
+            }
+          },
+          defaultRec: () => ({
+            id: '',
+            name: ''
+          })
+        },
+        crudOps: { // CRUD
+          export: async (payload) => {
+          },
+          find: async (payload) => {
+            let records = []
+            let totalRecords = 0
+            const { pagination } = payload // filterData
+            // const { page, rowsPerPage } = pagination // sortBy, descending
+            // console.log('TOREMOVE', page, rowsPerPage)
+            // GrqphQL
+            try {
+              const rv = await apolloClient.query({ query: GET_CATEGORIES })
+              records = rv.data.getCategories.results
+              totalRecords = rv.data.getCategories.total
+              // console.log('ABC', rv.data.getCategories)
+            } catch (e) {
+              console.log(e)
+            }
+            // REST
+            // try {
+            //   const { data: { results, total } } = await http.get('/api/categories', {
+            //     params: {
+            //       page: page > 0 ? page - 1 : 0,
+            //       limit: rowsPerPage
+            //     }
+            //   })
+            //   // console.log(results)
+            //   records = results
+            //   totalRecords = total
+            // } catch (e) {
+            //   console.log(e)
+            // }
+            return { records, pagination, totalRecords }
+          },
+          findOne: async (payload) => {
+            const { id } = payload
+            // GrqphQL
+            try {
+              const rv = await apolloClient.query({ query: GET_CATEGORY, variables: { id: parseInt(id) } })
+              // console.log('rv', rv)
+              return rv.data.getCategory
+            } catch (e) { }
+            // REST
+            // try {
+            //   const { data } = await http.get(`/api/categories/${id}`)
+            //   return data
+            // } catch (e) { }
+            // return { }
+          },
+          create: async (payload) => {
+            try {
+              let { record: { id, ...noIdData } } = payload
+              // const rv =
+              await http.post('/api/categories', noIdData)
+              // console.log(rv)
+            } catch (e) {
+              return 500
+            }
+            // return { // EXAMPLE return object with code property omitted
+            //   ok: true,
+            //   msg: 'OK'
+            // }
+            return 201
+          },
+          update: async (payload) => {
+            // GrqphQL
+            // console.log(payload)
+            try {
+              let { record: { id, ...noIdData } } = payload
+              console.log(noIdData)
+              // const rv =
+              await apolloClient.mutate({
+                mutation: PATCH_CATEGORY,
+                variables: {
+                  id: parseInt(id),
+                  body: {
+                    name: noIdData.name
+                  }
+                }
+              })
+              // console.log('rv', rv)
+            } catch (e) {
+              if (parseInt(e.message) === 409) return 409
+              else return 500
+            }
+            // REST
+            // try {
+            //   let { record: { id, ...noIdData } } = payload
+            //   const rv = await http.patch(`/api/categories/${id}`, noIdData)
+            //   console.log(rv)
+            //   // if (!doc.exists) throw new Error(409)
+            //   // if (await hasDuplicate('party', 'name', noIdData['name'], id)) throw new Error(409)
+            //   // await t.set(docRef, noIdData)
+            // } catch (e) {
+            //   if (parseInt(e.message) === 409) return 409
+            //   else return 500
+            // }
+            return 200
+          },
+          delete: null // TBD if delete, must also delete all dependancies, move all buttons to right?
+
+        }
+      }
+    }
+  }
+}
+
+</script>
--- example-rest/src/router/index.js
+++ example-rest/src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import AuthGuard from './auth-guard'
 
 import * as authorDefs from '@/components/author'
-import * as categoryDefs from '@/components/category'
+// import * as categoryDefs from '@/components/category'
 
 import VueCrudX from '../../../src/VueCrudX' // Component shared between projects
 // const VueCrudX = Vue.component('vue-crud-x') // does not work!
@@ -20,17 +20,18 @@ export default new Router({
       beforeEnter: AuthGuard,
       props: (route) => ({ storeName: route.name, parentId: route.params.parentId || null, ...authorDefs })
     },
-    {
-      path: '/categories',
-      name: 'categories',
-      component: VueCrudX,
-      beforeEnter: AuthGuard,
-      props: (route) => ({ storeName: route.name, parentId: route.params.parentId || null, ...categoryDefs })
-    },
+    // {
+    //   path: '/categories',
+    //   name: 'categories',
+    //   component: VueCrudX,
+    //   beforeEnter: AuthGuard,
+    //   props: (route) => ({ storeName: route.name, parentId: route.params.parentId || null, ...categoryDefs })
+    // },
     // route level code splitting, components are lazy loaded using import
     { path: '/dashboard', name: 'dashboard', component: () => import('@/pages/Dashboard'), beforeEnter: AuthGuard },
     { path: '/books', name: 'books', component: () => import('@/pages/Book'), beforeEnter: AuthGuard },
     { path: '/books/:id/pages', name: 'pages', component: () => import('@/pages/Page'), beforeEnter: AuthGuard },
+    { path: '/categories', name: 'categories', component: () => import('@/pages/Category'), beforeEnter: AuthGuard },
     { path: '/signup', name: 'SignUp', component: () => import('@/pages/SignUp') },
     { path: '/', name: 'SignIn', component: () => import('@/pages/SignIn') },
     { path: '*', redirect: '/' }
