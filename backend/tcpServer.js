5604a3854f7e29679c760937925307f46db9d5d6
Aaron Gong <ais-one@users.noreply.github.com>
2019-12-28 02:36:56 -0800

    0.2.6 (#79)
    * v0.2.6 initial commit
    
    * update readme to include v1 article
    
    * quick fixes see README.md Notices & Updates
    
    * update packages
    
    * updated packages
    
    * update packages
    
    * update packages
    
    * update packages
    
    * update packages
    
    * upgrade to objection

---
+++ backend/tcpServer.js
@@ -0,0 +1,32 @@
+const net = require('net');
+const port = 7070;
+const host = '127.0.0.1';
+
+const server = net.createServer();
+server.listen(port, host, () => {
+    console.log('TCP Server is running on port ' + port + '.');
+});
+
+let sockets = [];
+
+server.on('connection', function(sock) {
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sockets.push(sock);
+
+    sock.on('data', function(data) {
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to all the connected, the client will receive it as data from the server
+        sockets.forEach(function(sock, index, array) {
+            sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
+        });
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        let index = sockets.findIndex(function(o) {
+            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+        })
+        if (index !== -1) sockets.splice(index, 1);
+        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+    });
+});
