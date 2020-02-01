eed7808dc03cf59f3dd0a44717dee58d7ec6a106
Aaron Gong <aaronjxz@gmail.com>
2020-02-01 10:25:03 +0800

    wip

---
+++ deploy/example-spa.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PEM=../deploy/id_rsa
+URL=user@127.0.0.1
+
+# cd web/example-spa
+# npm run build
+# tar -zcvf deploy-www.tgz dist
+# scp -i $PEM deploy-www.tgz $URL:~ && rm deploy-www.tgz
+# ssh -i $PEM $URL "tar -zxvf deploy-www.tgz -C ~/app;rm deploy-www.tgz"
+
+read # pause exit in windows
