0ec6daeb15124f2e983be7710e3b0e2f2ce29416
Aaron Gong <aaronjxz@gmail.com>
2020-02-02 06:11:25 +0800

    wip

--- deploy/example-app/default.sh
+++ deploy/example-app/default.sh
@@ -1,41 +1,52 @@
 # npm run deploy -- --port=1337
 #!/bin/bash
 PEM=../deploy/id_rsa
-URL=ubuntu@127.0.0.1
+# URL=ubuntu@127.0.0.1
+URL=root@128.199.139.34
 
 # echo $PEM $URL
 # echo --- $PEM $URL --- $1 $2
+# ACTION=$2
 
-if [ "$2" == "ssh" ]; then
-    # ssh -i $PEM $URL -L 27000:127.0.0.1:27017
+echo "Type in one of the actions below and press enter"
+echo "ssh = connect via ssh"
+echo "be = deploy backend"
+echo "fe = build and deploy frontend"
+echo "list = list daemons"
+echo "start = start daemons"
+echo "stop = stop daemons"
+read ACTION
+
+
+# ssh -i $PEM $URL -L 27000:127.0.0.1:27017
+if [ "$ACTION" == "ssh" ]; then
     ssh $URL -L 27000:127.0.0.1:27017
-elif [ "$2" == "fe" ]; then
+# deploy frontend
+elif [ "$ACTION" == "fe" ]; then
     echo "Deploy Front End"
     cd ~/web/example-spa
     npm run build
     tar -zcvf deploy-www.tgz dist
     scp -i $PEM deploy-www.tgz $URL:~ && rm deploy-www.tgz
     ssh -i $PEM $URL "tar -zxvf deploy-www.tgz -C ~/app;rm deploy-www.tgz"
-elif [ "$2" == "be" ]; then
+# deploy backend
+elif [ "$ACTION" == "be" ]; then
     echo "Deploy Back End"
     cd ~/app
     tar --exclude="node_modules,coverage,example-app/tests" -zcvf deploy-app.tgz .
     scp -i $PEM deploy-app.tgz $URL:~ && rm deploy-app.tgz
     ssh -i $PEM $URL "tar -zxvf deploy-app.tgz -C ~/app;rm deploy-app.tgz"
-elif [ "$2" == "list" ]; then
+# pm2 stuff
+elif [ "$ACTION" == "list" ]; then
     ssh -i $PEM $URL "pm2 list"
-elif [ "$2" == "start" ]; then
-    ssh -i $PEM $URL "cd ~/api; authbind --deep pm2 start --only api --env production;"
-elif [ "$2" == "stop" ]; then
-    ssh -i $PEM $URL "cd ~/api; pm2 delete api;"
+elif [ "$ACTION" == "start" ]; then
+    ssh -i $PEM $URL "cd ~/app; authbind --deep pm2 start --only app,cron --env production;"
+elif [ "$ACTION" == "stop" ]; then
+    ssh -i $PEM $URL "cd ~/app; pm2 delete app cron;"
+# fallback
 else
-    echo "Command not found"
-    echo "npm run deploy ssh = connect via ssh"
-    echo "npm run deploy be = deploy backend"
-    echo "npm run deploy fe = build and deploy frontend"
-    echo "npm run deploy list = list daemons"
-    echo "npm run deploy start = start daemons"
-    echo "npm run deploy stop = stop daemons"
+    echo "Action not found"
 fi
 
+echo "Done... press enter to exit"
 read # pause exit in windows
