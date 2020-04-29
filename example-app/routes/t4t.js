92f470693aa58e9eaf257ad40040d65c62d0dd46
Aaron Gong <aaronjxz@gmail.com>
2020-04-30 02:30:11 +0800

    wip

--- example-app/routes/t4t.js
+++ example-app/routes/t4t.js
@@ -3,37 +3,58 @@ const express = require('express')
 const Model = require('../../common-app/database')
 const knex = Model.knex()
 
+// const mongo = require('../../common-app/mongo')
+// const ObjectID = require('mongodb').ObjectID
+
 const csvParse = require('csv-parse')
+
 // const { authUser } = require('../middlewares/auth')
+
 const t4tCfg = {
-  'books': { // table alias
-    name: 'books', // table name
-    id: 'id', // primary key name (if any)
-    cols: {
-      id: {
-
-      },
-      name: {
-        key: true
-      },
-      categoryId: {
-        // related
-      },
-      rating: {
-
-      },
-      yearPublished: {
-
-      },
-      created_at: {
-
+  db: 'knex', // knex / mongodb
+  tables: {
+    'books': { // table alias
+      name: 'books', // table name
+      id: 'id', // primary key name (if any)
+      cols: {
+        id: {
+          auto: 'id'
+        },
+        name: {
+          key: true,
+          type: 'string'
+        },
+        categoryId: {
+          // related
+        },
+        rating: {
+          type: 'integer'
+        },
+        yearPublished: {
+          type: 'string'
+        },
+        created_at: {
+          auto: 'ts'
+        }
       }
+      // {id: 1, name: 'book1', categoryId: 1, rating: 5, yearPublished: '2004', created_at: mkDt() },
     }
-    // {id: 1, name: 'book1', categoryId: 1, rating: 5, yearPublished: '2004', created_at: mkDt() },
+  },
+  allColName: function (name) { this.countCols(name, true) },
+  nonAutoColNames: function (name) { this.countCols(name, false) },
+  // autoCols
+  countCols: function (name, includeAuto = false) {
+    const cols = this.tables[name].cols
+    // Object.keys(cols)
+    let count = []
+    for (let key in cols) {
+      if (!cols[key].auto || includeAuto) count.push(key)
+    }
+    return count
   }
 }
 
-const table = t4tCfg['books']
+const table = t4tCfg.tables['books']
 
 function formUniqueKey(table, args) {
   const where = {}
@@ -48,45 +69,67 @@ function formUniqueKey(table, args) {
 
 module.exports = express.Router()
   .get('/t4t/:table', asyncWrapper(async (req, res) => {
-    res.json() // return the table info...
+    res.json(t4tCfg) // return the table info...
   }))
   .get('/t4t/:table/find', asyncWrapper(async (req, res) => {
-    const { page = 1, limit = 2 } = req.query
+    const { page = 1, limit = 2, ...filters } = req.query
     const rv = { results: [], total: 0 }
+
+    // knex
     const query = knex(table.name)
     let total = await query.clone().count()
     rv.total = Object.values(total[0])[0]
     // tbd where, sort
     rv.results = await query.clone().limit(limit).offset((page > 0 ? page - 1 : 0) * limit)
+
+    // mongo
+    // const projection = {}
+    // rv.total = await mongo.db.collection(table.name).find(filter, projection).count()
+    // rv.results = await mongo.db.collection(table.name).find(filter, projection).sort().skip( (Number(page) - 1) * Number(limit) ).limit(Number(limit)).toArray()
+
     res.json(rv)
   }))
   .get('/t4t/:table/find-one/:id?', asyncWrapper(async (req, res) => {
     const where = req.params.id ? { id: req.params.id } : formUniqueKey(table, req.query)
     if (!where) return res.status(400).json() // bad request
+    // knex
     const rv = await knex(table.name).where(where).first()
+    // mongodb
+    // const rv = await mongo.db.collection(table.name).findOne(where) // { _id: new ObjectID(id) }
     return res.json(rv)
   }))
   .patch('/t4t/:table/update/:id?', asyncWrapper(async (req, res) => {
     const where = req.params.id ? { id: req.params.id } : formUniqueKey(table, req.query)
     if (!where) return res.status(400).json() // bad request
-    const rv = await knex(table.name).update({ ...req.body }).where(where)
+    // knex
+    const rv = await knex(table.name).update(req.body).where(where)
+    // mongodb
+    // await mongo.db.collection(table.name).updateOne(where, { $set: req.body })
     res.json(rv)
   }))
   .post('/t4t/:table/create', asyncWrapper(async (req, res) => {
+    // knex
     const rv = await knex(table.name)
-      .returning('id')
+      // .returning('id')
       .insert(req.body)
+    // mongodb
+    // const rv = await mongo.db.collection(table.name).insertOne(req.body) // rv.insertedId, rv.result.ok
     res.json(rv)
   }))
   .delete('/t4t/:table/delete/:id', asyncWrapper(async (req, res) => {
     const where = req.params.id ? { id: req.params.id } : formUniqueKey(table, req.query)
     if (!where) return res.status(400).json() // bad request
+    // knex
     await knex(table.name).where(where).delete()
+    // await mongo.db.collection(table.name).deleteOne(where)
     res.json()
   }))
   .delete('/t4t/:table/delete-many', asyncWrapper(async (req, res) => {
     const ids = JSON.parse(req.query.ids) // [1,2,3]
+    // knex
     await knex(table.name).whereIn(id, ids).delete()
+    // mongodb - TBD
+    // await mongo.db.collection(table.name).delete(where)
     res.json()
   }))
 
