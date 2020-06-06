c9c5c1d8ce4b50cad54af7abb09eff1deec7a3fb
Aaron Gong <aaronjxz@gmail.com>
2020-06-06 11:33:12 +0800

    wip

--- app.js
+++ app.js
@@ -17,14 +17,11 @@ const { USE_HTTPS, httpsCerts } = config
 const server = USE_HTTPS ? https.createServer(httpsCerts, app) : http.createServer(app)
 
 require('./common-app/express/services')(server, app, config)
-
 require('./common-app/express/preroute')(app, config) // use as early as possible
 // PASSPORT - we do not need passport except if for doing things like getting SAML token and converting it to JWT token (see common-app folder for saml)
-
 require(`./${APPNAME}/router`)(app) // route setup
 const { USE_GRAPHQL } = config
 USE_GRAPHQL ? require(`./${APPNAME}/graphql`)(app, server) : null // add graphql here
-
 require('./common-app/express/postroute')(app, express, config) //require after routes setup
 
 // // Create and Deploy Your First Cloud Functions
