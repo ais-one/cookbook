69128b5bbb8365622a72ca0cb569b5248cd3402e
Aaron Gong <aaronjxz@gmail.com>
2020-07-19 23:23:55 +0800

    wip

--- example-app/router/auth.js
+++ example-app/router/auth.js
@@ -7,6 +7,33 @@ module.exports = express.Router()
   .post('/signup', authController.signup)
   .post('/check-github', authController.checkGithub)
   .get('/logout', authUser, authController.logout)
+  /**
+   * @swagger
+   * /api/auth/login:
+   *    post:
+   *      tags:
+   *        - "Auth"
+   *      description: Login
+   *      responses:
+   *        '200':
+   *          description: OK
+   *          content:
+   *            application/json:
+   *              schema:
+   *                properties:
+   *                  token:
+   *                    type: string
+   *                    example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ._l3MQQAq4Py1q9eWoaLomaX8wvSkYNiztEK_OZ1qlFA"
+   *                  refresh_token:
+   *                    type: integer
+   *                    example: 1234567890123
+   *        '401':
+   *          description: Unauthorized
+   *        '403':
+   *          description: Forbidden
+   *        default:
+   *          description: Unexpected error
+   */
   .post('/login', authController.login)
   .post('/refresh', authUser, authController.refresh)
   .post('/otp', authUser, authController.otp)
