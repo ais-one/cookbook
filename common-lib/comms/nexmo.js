952d04bae0ad8b879945cb350202e31ee9d18a93
Aaron Gong <aaronjxz@gmail.com>
2020-07-25 18:34:11 +0800

    fix nexmo

--- common-lib/comms/nexmo.js
+++ common-lib/comms/nexmo.js
@@ -3,31 +3,40 @@ const axios = require('axios')
 const { NEXMO_KEY, NEXMO_SECRET, NEXMO_FROM = 'SMSnotice' } = global.CONFIG
 
 const nexmo = {
+  // sms = 6511112222
+  // one at a time...
   send: async function (sms, message, from = NEXMO_FROM) {
     try {
-      return await axios.get(`https://rest.nexmo.com/sms/json?api_key=${NEXMO_KEY}&api_secret=${NEXMO_SECRET}&to=${sms}&from=${from}&text=${message}`)
-    } catch (e) {
-      return null
-    }
-  },
-  ismsSend: async function (sms, message, from = NEXMO_FROM) {
-    try {
-      const url = 'https://sms.era.sg/isms_mt.php?'
-      const options = { 
-        params: {
-          uid: NEXMO_KEY,
-          // pwd: 'xxxxxxxxyyyyyyyyzzzzzzzzxxxxxxxx',
-          pwd: md5( NEXMO_SECRET ),
-          dnr: sms,
-          snr: from,
-          msg: message,
-          split: 5
-        }
+      if (sms && message) {
+        return await axios.get(`https://rest.nexmo.com/sms/json?api_key=${NEXMO_KEY}&api_secret=${NEXMO_SECRET}&to=${sms}&from=${from}&text=${message}`)
       }
-      return await axios.get(url, options)    
     } catch (e) {
-      return null
+      console.log('send', e.toString())      
     }
+    return null
+  },
+  // sms = 6511112222
+  // one at a time...
+  ismsSend: async function (sms, message, from = NEXMO_FROM) {
+    const url = 'https://sms.era.sg/isms_mt.php?'
+    try {
+      if (sms && message) {
+        const options = { 
+          params: {
+            uid: NEXMO_KEY,
+            pwd: require('crypto').createHash('md5').update( NEXMO_SECRET ).digest('hex'),
+            dnr: sms,
+            snr: from,
+            msg: message,
+            split: 5
+          }
+        }
+        return await axios.get(url, options)
+      }
+    } catch (e) {
+      console.log('ismsSend', e.toString())      
+    }
+    return null
   }
 }
 
