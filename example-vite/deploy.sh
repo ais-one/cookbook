2a0125e19f19022f62f803f3d6afc2837afd073f
Aaron Gong <aaronjxz@gmail.com>
2020-10-10 13:13:32 +0800

    wip

--- example-vite/deploy.sh
+++ example-vite/deploy.sh
@@ -32,8 +32,8 @@ echo "build and deploy - site $SITE ($1)"
 # CANNOT because need vite to build  npm ci --only=production
 # npm install && npm cache clean --force
 npm install
-npm run vite build --mode $1
-# npm run build-$1
+# npm run vite build --mode $1
+npm run build-$1
 
 if [ "$CI" = "true" ]; then
   echo "CI configured gcloud auth"
