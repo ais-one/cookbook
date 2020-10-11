fff6c96dfbd69dfe928f514a39157d4d6a07f965
Aaron Gong <aaronjxz@gmail.com>
2020-10-12 00:43:50 +0800

    add pwa sw example (commented) - lint vite

--- example-vite/src/lib/esm/saml.js
+++ example-vite/src/lib/esm/saml.js
@@ -5,17 +5,21 @@ export default class Saml {
     this._token = ''
     this._payload = null
   }
-  val() { // try catch
+
+  val() {
+    // try catch
     // let token = window.location.hash.substring(1) // (new URL(window.location.href)).searchParams.get('token')
     // if (!token) this.getFromLocalStorage()
     // if (!token) this.login()
     // else thi.setToken(token)
   }
-  login () {
-    const redirect = (window.location.protocol + window.location.hostname) + (window.location.port === 443 || window.location.port === 80) ? '' : window.location.port + '/callback-url'
-    const goto = 'http://127.0.0.1:3000' + `/api/saml/login?redirect_to=${goto}&groups=&expiry=`
+
+  login() {
+    const redirect = window.location.protocol + window.location.hostname + (window.location.port === 443 || window.location.port === 80) ? '' : window.location.port + '/callback-url'
+    const goto = 'http://127.0.0.1:3000' + `/api/saml/login?redirect_to=${redirect}&groups=&expiry=`
     window.location.assign(goto)
   }
+
   // logout - cleartoken - window.location.replace('https://adfc.test.com/adfs/ls/?wa=wsignout1.0&wreply='+ encodeURIComponent(LOGOUT_URL))
 }
 
@@ -26,4 +30,3 @@ export default class Saml {
 //   if (abc._token) {
 //     this.payload = abc._payload
 //     this.token = abc._token  }
-
