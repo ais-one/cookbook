241e28448bb350bf1b1be6925b68829ecbc8d9ba
Aaron Gong <aaronjxz@gmail.com>
2020-11-28 10:02:03 +0800

    fix action

--- example-app/update.sh
+++ example-app/update.sh
@@ -4,5 +4,9 @@ cp ../common-lib/esm/t4t-validate.js ./lib/esm
 cp ../common-lib/esm/datetime.js ./lib/esm
 cp ../common-lib/esm/sleep.js ./lib/esm
 cp ../common-lib/esm/util.js ./lib/esm
-echo "Copying done... press enter to continue"
-read
+
+echo Done...
+if [ "$CI" != "true" ]; then
+  echo press enter key to exit
+  read # pause exit in windows
+fi
