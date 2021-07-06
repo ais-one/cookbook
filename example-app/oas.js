09b17ad9d8c0de321df5e4c078480dd733b0b32d
Aaron Gong <aaronjxz@gmail.com>
2021-07-06 08:48:15 +0800

    wip swagger

---
+++ example-app/oas.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./openapi/petstore.yaml')
+// const swaggerDocument = require('./swagger.json')
+
+const options = {
+  explorer: true
+}
+
+app.use('/openapi', express.static('openapi'))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options))
+app.get('*', (req, res) => {
+  res.send('use /api-docs!')
+})
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`)
+})
+
+
+// if serving multiple documents
+// const swaggerDocumentOne = require('./swagger-one.json');
+// const swaggerDocumentTwo = require('./swagger-one.json');
+// app.use('/api-docs-one', swaggerUi.serveFiles(swaggerDocumentOne, options), swaggerUi.setup(swaggerDocumentOne));
+// app.use('/api-docs-two', swaggerUi.serveFiles(swaggerDocumentTwo, options), swaggerUi.setup(swaggerDocumentTwo));
+
+// load from multiple URLs
+// var options = {
+//   explorer: true,
+//   swaggerOptions: {
+//     urls: [
+//       { url: 'http://petstore.swagger.io/v2/swagger.json', name: 'Spec1' },
+//       { url: 'http://petstore.swagger.io/v2/swagger.json', name: 'Spec2' }
+//     ]
+//   }
+// }
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(null, options));
--- example-app/sandbox/rs232/index.js
+++ example-app/sandbox/rs232/index.js
@@ -1,5 +1,15 @@
 const SerialPort = require('serialport')
-const port = new SerialPort('COM1', {
+
+console.log()
+
+comPort = process.argv[2]
+
+if (!comPort) {
+  console.log('Please specify com port in command argument, e.g. rs232.exe COM2')
+  process.exit(1)
+}
+
+const port = new SerialPort(comPort, {
   baudRate: 57600,
   autoOpen: false,
   /* default
@@ -24,7 +34,7 @@ port.open(function (err) {
     return console.log('Error opening port: ', err.message)
   }
   // Because there's no callback to write, write errors will be emitted on the port:
-  port.write('main screen turn on')
+  port.write('Port Open...')
 })
 
 // The open event is always emitted
@@ -33,9 +43,9 @@ port.on('open', function() {
 })
 
 // Read data that is available but keep the stream in "paused mode"
-port.on('readable', function () {
-  console.log('Data:', port.read())
-})
+// port.on('readable', function () {
+//   console.log('Data:', port.read())
+// })
 
 // Switches the port into "flowing mode"
 port.on('data', function (data) {
@@ -62,4 +72,6 @@ let aaa = require(loc);
 
 npx pkg --targets win ./bin/www
 npx pkg --targets linux ./bin/www
+
+npx pkg index.js --targets node14-win-x64 --output rs232.exe
 */
