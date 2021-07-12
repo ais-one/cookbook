4fd711a5469d8fbc36e2947f8645e49748b40f04
Aaron Gong <aaronjxz@gmail.com>
2021-07-13 03:21:03 +0800

    wip

--- example-app/app-custom/routes/index.js
+++ example-app/app-custom/routes/index.js
@@ -2,6 +2,11 @@
 
 const categories = require('./categories')
 
+
+// const websocket = require('@es-labs/node/services/websocket') // .open(null, null) // or set to null
+// websocket.setOnClientMessage = async (message, ws, _wss) => { }
+// websocket.setOnClientCLose =  (ws) => { }
+
 module.exports = (app) => {
   app.use(`/api/app-custom`, categories )
 }
