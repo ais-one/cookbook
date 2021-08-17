8bce50eee906bb1220a782246e5e5b791f491881
Aaron Gong <aaronjxz@gmail.com>
2021-08-17 12:55:35 +0800

    fido2 wip

--- js-node/expressjs/router/fido2.js
+++ js-node/expressjs/router/fido2.js
@@ -4,6 +4,29 @@ const base64url = require('base64url') // TOREMOVE in Node 16 LTS - use Buffer.f
 
 const { Fido2Lib } = require("fido2-lib")
 
+/*
+const test = 'dUNUFx6OPh829nRG9J7JK4wMZ4QvUddT5XUUHIf_jH0'
+
+// NODEJS
+const b64_b64url = (inStr) => inStr.replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '')
+const b64url_b64 = (inStr) => inStr.replace(/\-/g, '+').replace(/_/g, '/') + '='.repeat((inStr.length % 4) ? 4 - (inStr.length % 4) : 0)
+const b64_ab = (inStr) => Buffer.from(inStr, 'base64')
+const ab_b64 = (buf) => Buffer.from(buf).toString('base64')
+
+const b64_ab = window ? (inStr) => Uint8Array.from(atob(inStr), c => c.charCodeAt(0)) : Buffer.from(inStr, 'base64') // base64 encoded utf8
+const ab_b64 = window ? (buf) => btoa(buf.reduce((data, val)=> data + String.fromCharCode(val), '')) : Buffer.from(buf).toString('base64') // Uint8Array or Buffer
+
+
+
+const b64 = b64url_b64(test) // convert base64url to base64
+const ab = Buffer.from(b64, 'base64') // convert base64 to array buffer
+console.log('ab', ab)
+
+const b64_2 = Buffer.from(ab).toString('base64') // ab.toString('base64') // convert arraybuffer to base64
+const b64url = b64_b64url(b64_2)// convert base64 to base64url
+console.log('b64url', b64url)
+*/
+
 // could also use one or more of the options below,
 // which just makes the options calls easier later on:
 const f2l = new Fido2Lib({
